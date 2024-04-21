48967a1a01955f1cabf5580a3408e3682fb10eb1
Ashraf Alagmawy <ashrafemad097@gmail.com>
2024-04-21 18:12:08 +0200

    Working on login and register page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,31 @@
-const Login = () => {
-  return <div>Login</div>;
+import { Form, Button, Col, Row } from 'react-bootstrap';
+import { Heading } from '@components/common';
+
+const Register = () => {
+  return (
+    <>
+      <Heading title="User Login" />
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="text" name="email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" name="password" />
+            </Form.Group>
+
+            <Button variant="info" type="submit" style={{ color: 'White' }}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
-export default Login;
+export default Register;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,74 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { z } from 'zod';
+import { Form, Button, Col, Row } from 'react-bootstrap';
+import { Heading } from '@components/common';
+
+const signUpSchema = z.object({
+  firstName: z.string().min(1, { message: 'First name is required' }),
+  lastName: z.string().min(1, { message: 'Last name is required' }),
+  email: z.string().min(1, { message: 'Email address is required' }).email(),
+  password: z
+    .string()
+    .min(8, { message: 'Password must be at least 8 characters' }),
+  confirmPassword: z
+    .string()
+    .min(8, { message: 'Confirm Password must is required' }),
+});
+
+type TFormInputs = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const { register, handleSubmit } = useForm<TFormInputs>();
+
+  const submitForm: SubmitHandler<TFormInputs> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Heading title="User Registration" />
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form onSubmit={handleSubmit(submitForm)}>
+            <Form.Group className="mb-3">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="text" {...register('firstName')} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="text" {...register('lastName')} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="text" {...register('email')} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" {...register('password')} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" {...register('confirmPassword')} />
+            </Form.Group>
+
+            <Button variant="info" type="submit" style={{ color: 'White' }}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Register;
